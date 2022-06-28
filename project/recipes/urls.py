f12199aca740266646b2c719f5f305d3b9c8edcc
Douglas Teixeira <dougteixeira@hotmail.com>
2022-06-27 21:04:57 -0300

    recipes

--- project/recipes/urls.py
+++ project/recipes/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import home
+from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', home)
+    path('', home, name='home'),
+    path('recipes/category/<int:category_id>/', category, name='category'),
+    path('recipes/<int:id>/', recipe, name='recipe'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- project/recipes/views.py
+++ project/recipes/views.py
@@ -1,4 +1,48 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from .models import *
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={'nome':'Douglas'})
+    recipes = Recipe.objects.filter(
+        is_published = True
+    ).order_by('-id')
+
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
+
+def category(request, category_id):
+    """recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published = True,
+    ).order_by('-id')
+
+    if not recipes:
+        #return HttpResponse(content='Not found', status=404)
+        raise Http404('Not found🤡')"""
+    
+    #desse jeito não vai ordenar por ordem de criação
+    #recipes = get_list_or_404(Recipe, category__id=category_id, is_published = True) 
+
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id,
+        is_published = True,
+    ).order_by('-id')) 
+
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title_category': f'{recipes.first[0].category.name} Category |'
+    })
+
+
+def recipe(request, id):
+    #recipe = Recipe.objects.filter(id=id, is_published=True).first()
+
+    recipe = get_object_or_404(Recipe, id=id, is_published=True)
+
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': recipe,
+        'is_detail_page': True,
+    })
+
